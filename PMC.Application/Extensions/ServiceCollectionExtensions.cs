2de76bcd92ef689c36f1331aa9cd0a6d04b8d7d9
Francis Elendu <elcfran6@hotmail.com>
2024-10-05 00:23:13 -0500

    Initial structure commit

---
+++ PMC.Application/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using PMC.Application.Mapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMC.Application.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddApplication(this IServiceCollection services)
+        {
+            var applicationAssembly = typeof(ServiceCollectionExtensions).Assembly;
+
+            IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+            services.AddSingleton(mapper);
+
+        }
+    }
+}
---
+++ PMC.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PMC.Infrastructure.Persistence;
+
+namespace PMC.Infrastructure.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddInfrastructure(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<PrescriptionManagementDbContext>(options => options.UseSqlServer(config.GetConnectionString("DefaultConnection")));
+
+            //services.AddScoped<IRestaurantSeeder, RestaurantSeeder>();
+            //services.AddScoped<IRestaurantsRepository, RestaurantsRepository>();
+        }
+    }
+}
